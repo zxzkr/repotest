5ff3537eaa40d867c386034461b5914d3ac28c39
zzz <zzz@zxz.kr>
2022-04-21 07:49:05 +0000

    update for aws

---
+++ build/precache-manifest.466525b73cc51603b2f966fc6df39f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba62e4a4c447c70dbb2b1b923503477c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a66658b87f941509680f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879c1f8800d3fd79cc31",
+    "url": "/static/js/2.011518c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.011518c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66658b87f941509680f",
+    "url": "/static/js/main.17adc63b.chunk.js"
+  },
+  {
+    "revision": "21f27347af35e687715a",
+    "url": "/static/js/runtime-main.d5ab784a.js"
+  }
+]);
